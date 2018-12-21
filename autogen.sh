8a3a1152ca9e8a6de6b5a05864d00b9f8aa5d97e
roothere <38818888+adeptio-project@users.noreply.github.com>
2018-12-22 01:57:04 +0200

    check if autoreconf is installed

--- autogen.sh
+++ autogen.sh
@@ -7,5 +7,7 @@ if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
   export LIBTOOLIZE
 fi
+which autoreconf >/dev/null || \
+   (echo "configuration failed, please install autoconf first" && exit 1)
 /usr/bin/autoreconf --install --force --warnings=all 
 #end
