83d8daf0d31ebdae497a84b3042a194f980cce28
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-01-03 15:01:08 +0200

    Postpone masternode rejection until v2.1.0.0

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -235,7 +235,7 @@ void CMasternode::Check(bool forceCheck)
 
     // The "StorADE" service needs the correct default port to work properly
     if(!storADEserver::CheckStorADEport(addr)) {
-        activeState = MASTERNODE_STORADE_EXPIRED;
+        //activeState = MASTERNODE_STORADE_EXPIRED; // Postpone to v2.1.0.0
         LogPrintf("CMasternode::Check() - %s StorADEserver not in running state: rejecting masternode\n", addr.ToStringIP());
         return; 
     }
