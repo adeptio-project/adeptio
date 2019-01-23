08bd14bc775cea2eb053828bd733e9904f90795d
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-01-23 02:46:14 +0200

    include testnet zerocoin

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -325,7 +325,22 @@ public:
         strSporkKeyOld = "04bbba1f836f32de0c31049a96f6838b46d9119554752eaba90ef7df5370f6a54d9fbc1aa7250cf356f149be2e396b983e9d56a288ceafa1093744b3a4b72c4125";
         strObfuscationPoolDummyAddress = "";
         nStartMasternodePayments = 1525177401;
-        nBudget_Fee_Confirmations = 3;
+        /** Zerocoin */
+        zerocoinModulus = "25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784"
+            "4069182906412495150821892985591491761845028084891200728449926873928072877767359714183472702618963750149718246911"
+            "6507761337985909570009733045974880842840179742910064245869181719511874612151517265463228221686998754918242243363"
+            "7259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133"
+            "8441436038339044149526344321901146575444541784240209246165157233507787077498171257724679629263863563732899121548"
+            "31438167899885040445364023527381951378636564391212010397122822120720357";
+        nMaxZerocoinSpendsPerTransaction = 7; // Assume about 20kb each
+        nMinZerocoinMintFee = 1 * CENT; //high fee required for zerocoin mints
+        nMintRequiredConfirmations = 100; //the maximum amount of confirmations until accumulated in 19
+        nRequiredAccumulation = 1;
+        nDefaultSecurityLevel = 100; //full security level for accumulators
+        nZerocoinHeaderVersion = 6; //Block headers must be this version once zerocoin is active
+        nZerocoinRequiredStakeDepth = 200; //The required confirmations for a zADE to be stakable
+
+        nBudget_Fee_Confirmations = 6; // Number of confirmations for the finalization fee
     }
     const Checkpoints::CCheckpointData& Checkpoints() const
     {
