0a63e6af64074349a02e5c17a3aa94dfdc1cb318
Thomas Janshon <38818888+adeptio-project@users.noreply.github.com>
2019-03-15 14:10:30 +0200

    Increase version to v2.1.1

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70717;
+static const int PROTOCOL_VERSION = 70718;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70716;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70717;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70717;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
