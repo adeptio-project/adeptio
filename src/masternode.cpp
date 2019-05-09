e7792a368312b3c99bbc8e69ee9a122fafb91451
Thomas Janshon <38818888+adeptio-project@users.noreply.github.com>
2019-05-09 21:50:19 +0300

    start storade in v2.1.4.0

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -251,7 +251,7 @@ void CMasternode::Check(bool forceCheck)
 
             block_height = BlockReading->nHeight;
 
-        if(block_height >= 1475201) // Postpone to v2.2.0.0
+        if(block_height >= 0) // Start storADE in v2.1.4.0
 
             threads.create_thread(boost::bind(&CMasternode::CheckStorADEport, this));
     }
