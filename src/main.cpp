beaa7c2e743fa5b7044ce8f2ae67afaaa69331ec
Thomas Janshon <38818888+adeptio-project@users.noreply.github.com>
2019-05-16 17:28:47 +0300

    Masternode reward

--- src/main.cpp
+++ src/main.cpp
@@ -2176,21 +2176,12 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCou
             return 0;
     }
 
-    if (nHeight <= 300) {
+    if (nHeight <= 250) {
         ret = blockValue / 5;
-    } else if (nHeight < 300 && nHeight > 400) {
-        ret = blockValue / (100 / 30);
-    } else if (nHeight = 400 && nHeight > 500) {
-        ret = 50 * COIN;
-    } else if (nHeight < 500 && nHeight > 600) {
-        ret = blockValue / 2;
-    } else if (nHeight < Params().Zerocoin_Block_V2_Start()) {
-        return GetSeeSaw(blockValue, nMasternodeCount, nHeight);
+    } else if (nHeight < 250 && nHeight > 400) {
+        ret = blockValue / (100 / 100 + 0.50);
     } else {
-        //When zADE is staked, masternode only gets 2 ADE
-        ret = 3 * COIN;
-        if (isZADEStake)
-            ret = 2 * COIN;
+        ret = blockValue / (100 / 100 + 0.25);
     }
 
     return ret;
