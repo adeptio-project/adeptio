52ffb52f8bd0aeb08af512aacf15e30d3ddd6782
Thomas Janshon <38818888+adeptio-project@users.noreply.github.com>
2019-05-16 17:31:49 +0300

    Masternode reward 3

--- src/main.cpp
+++ src/main.cpp
@@ -2179,9 +2179,9 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCou
     if (nHeight <= 250) {
         ret = blockValue / 5;
     } else if (nHeight <= 400 && nHeight > 250) {
-        ret = blockValue / (100 / 100 + 0.50);
+        ret = blockValue / (100 / 70);
     } else {
-        ret = blockValue / (100 / 100 + 0.25);
+        ret = blockValue / (100 / 80);
     }
 
     return ret;
