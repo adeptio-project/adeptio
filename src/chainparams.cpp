b350720a2d698edccb02724a3392ade470d06e3d
Thomas Janshon <38818888+adeptio-project@users.noreply.github.com>
2019-05-17 18:14:40 +0300

    Postpone ZeroCoin V2

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -146,7 +146,7 @@ public:
         nBlockLastGoodCheckpoint = 0; //Last valid accumulator checkpoint 
         nBlockEnforceInvalidUTXO = 9999999; //Start enforcing the invalid UTXO's 
         nInvalidAmountFiltered = 268200*COIN; //Amount of invalid coins filtered through exchanges, that should be considered valid 
-        nBlockZerocoinV2 = 200; //!> The block that zerocoin v2 becomes active.
+        nBlockZerocoinV2 = 9999999; //!> The block that zerocoin v2 becomes active.
         nBlockDoubleAccumulated = 1050010; 
         nEnforceNewSporkKey = 1525158000; //!> Sporks signed after (GMT): Tuesday, May 1, 2018 7:00:00 AM GMT must use the new spork key 
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM 
