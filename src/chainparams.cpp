e6534f008210ee49c8e586256e6d607c410ee7f7
Thomas Janshon <38818888+adeptio-project@users.noreply.github.com>
2019-05-17 15:12:13 +0300

    Set additional param

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -136,17 +136,17 @@ public:
         strDeveloperFeePayee = "ASpLv9c3UTN6SZXYR29iW46xFGjHRRvPWf";
  
         /** Height or Time Based Activations **/ 
-        nLastPOWBlock = 46; 
+        nLastPOWBlock = 250; 
         nModifierUpdateBlock = 9999999; 
-        nZerocoinStartHeight = 9999999; // 9M 
-        nZerocoinStartTime = 1597858248; 
+        nZerocoinStartHeight = 9999999; // 9,9M 
+        nZerocoinStartTime = 1599999999; 
         nBlockEnforceSerialRange = 9999999; //Enforce serial range starting this block 
         nBlockRecalculateAccumulators = 9999999; //Trigger a recalculation of accumulators 
         nBlockFirstFraudulent = 9999999; //First block that bad serials emerged 
         nBlockLastGoodCheckpoint = 0; //Last valid accumulator checkpoint 
         nBlockEnforceInvalidUTXO = 9999999; //Start enforcing the invalid UTXO's 
         nInvalidAmountFiltered = 268200*COIN; //Amount of invalid coins filtered through exchanges, that should be considered valid 
-        nBlockZerocoinV2 = 46; //!> The block that zerocoin v2 becomes active - roughly Tuesday, May 8, 2018 4:00:00 AM GMT 
+        nBlockZerocoinV2 = 200; //!> The block that zerocoin v2 becomes active.
         nBlockDoubleAccumulated = 1050010; 
         nEnforceNewSporkKey = 1525158000; //!> Sporks signed after (GMT): Tuesday, May 1, 2018 7:00:00 AM GMT must use the new spork key 
         nRejectOldSporkKey = 1527811200; //!> Fully reject old spork key after (GMT): Friday, June 1, 2018 12:00:00 AM 
