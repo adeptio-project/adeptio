08616d52ef9b40935ce154307a753020c518f8af
roothere <38818888+adeptio-project@users.noreply.github.com>
2019-06-04 11:16:31 +0300

    Masternode update script conf 4

--- scripts/update_masternode_version.sh
+++ scripts/update_masternode_version.sh
@@ -69,7 +69,8 @@ if [ "$OS_version" -eq "1" ]; then
 	wget https://github.com/adeptio-project/adeptio/releases/download/$ver_to_download/adeptio-$ver_to_download-x64-linux.zip
 	echo -e "${GREEN}3/5 New Adeptio wallet is now downloaded... Running for next task...${NC}"
 	unzip -o adeptio*.zip
-	sudo cp -fr ./adeptio-${ver_to_download}/bin/adeptio-cli ./adeptio-${ver_to_download}/bin/ /usr/bin/
+	sudo cp -fr ./adeptio-${ver_to_download}/bin/adeptiod /usr/bin/
+	sudo cp -fr ./adeptio-${ver_to_download}/bin/adeptio-cli /usr/bin/
 	echo -e "${GREEN}4/5 Adeptio wallet is updated... Running for next task...${NC}"
 	rm -fr ./adeptio-${ver_to_download}/ ; rm -fr adeptio*.zip
 	sudo systemctl start adeptiocore.service && sleep 30
